6268dbd115c40938932589b9249d4287996cb477
Maxi <maxi.poellinger@gmail.com>
2024-04-25 18:50:31 +0200

    added backtracking to get actual value; added config options via user input

--- rucksack-fptas/src/main.rs
+++ rucksack-fptas/src/main.rs
@@ -1,11 +1,27 @@
-use core::cmp::min;
+use std::io::{stdin, stdout, Write};
+use core::str::FromStr;
+
+
+fn userinput_or_default<T: FromStr>(default: T, preamble: &str) -> T {
+    let mut userinput = String::new(); 
+    
+    print!("{}", preamble);
+    stdout().flush().expect("Error flushing stdout!"); 
+
+    stdin().read_line(&mut userinput).expect("Failed to read user input!");
+    match userinput.trim().parse() {
+        Ok(res) => res,
+        Err(_) => {
+            default
+        }
+    }
+}
 
 fn main() {
     const N: usize = 8;
-    const EPSILON: f32 = 0.1;
     const B: usize = 645;
 
-    let objects_: [(usize, usize); N] = 
+    let objects: Vec<(usize, usize)> = 
     [
     // Weight, Price
         (153, 232),
@@ -16,24 +32,21 @@ fn main() {
         (137, 79),
         (148, 48),
         (249, 38)
-    ];
-
-    let objects: Vec<(usize, usize)> = objects_.into_iter().collect();
-
-    //let weights_orig: Vec<usize> = objects.clone().into_iter().map(|(w,_)| {w}).collect();
-    let prices_orig: Vec<_> = objects.iter().map(|(_,p)| {p}).collect();
+    ].into_iter().collect();
+    
+    let epsilon: f32 = 0.1;
+    
+    let prices_orig: Vec<_> = objects.clone().into_iter().map(|(_,p)| {p}).collect();
 
     let p_min = prices_orig.iter().min().expect("Error while finding min price");
     let p_max = prices_orig.iter().max().expect("Error while finding max price");
     let p_sum = prices_orig.clone().into_iter().sum::<usize>();
 
-    let k: f32 = 1.0; // EPSILON * *p_max as f32 / N as f32;
+    let k: f32 = userinput_or_default(epsilon * *p_max as f32 / N as f32, "Input custom value for scaling constant 'k' or leave empty for default value: ");
 
-    println!("N: {N}; min price: {p_min}; max price: {p_max}; sum: {p_sum}");
-    println!("EPSILON: {EPSILON} => k = EPSILON * p_max / N = {k}");
-
-    println!("\nBefore Conversion: ");
-    print_objects(&objects);
+    println!("Constants:");
+    println!("    min price: {p_min}; max price: {p_max}; sum: {p_sum}");
+    println!("    N: {N}; B: {B}; ε: {epsilon}; k: {k}");
 
     // conversion
     let objects: Vec<(usize, usize)> = objects.into_iter().map(|(w,p)| {
@@ -45,29 +58,50 @@ fn main() {
 
     let p_sum_adjusted = prices.clone().iter().sum::<usize>();
 
-    println!("\nAfter Conversion: ");
-    print_objects(&objects);
+    println!("\nObjects:");
+    print!("         | ");
+    for i in 0..objects.len() { print!("{:>5} ", i+1); }
+    print!("\n{:-<1$}", "", 10+6*objects.len());
+    print!("\nWeights  | ");
+    for w in &weights { print!("{:>5} ", *w); }
+    print!("\nPrices   | ");
+    for p in &prices_orig { print!("{:>5} ", *p); }
+    print!("\nAdj. Pr. | ");
+    for p in &prices { print!("{:>5} ", *p); }
+    println!("\n");
 
-    println!("\nNew sum: {p_sum_adjusted}");
+    print!("Print graph? y/N");
+    stdout().flush().expect("Error while flushing stdout!");
+    let mut userinput = String::new();
+    stdin().read_line(&mut userinput).expect("Error while reading user input!");
+    println!("");
+    let ui_trimmed = userinput.trim();
+
+    let print_graph: bool = ui_trimmed == "y" || ui_trimmed == "yes";
 
     // Start of algorithm
+    println!("Calculating solution...");
     let mut alpha_max: usize = 0;
 
     println!("\n");
-    println!("Matrix: {}x{}\n", p_sum_adjusted + 1, N);
-
-    print!("      | ");
-    for i in 0..N { print!("{:>5} ", i+1); }
-    println!(" | a_max");
-    println!("{:-<1$}", "", 8 + 6*N + 8);
+    if print_graph {
+        println!("Matrix: {}x{}\n", p_sum_adjusted, N);
+        print!("         | ");
+        for i in 0..N { print!("{:>5} ", i+1); }
+        println!(" | a_max");
+        println!("{:-<1$}", "", 11 + 6*N + 8);
+    }
 
     let num_or_inf = |n_opt: Option<usize>| { match n_opt { None => "∞".to_string(), Some(n) => n.to_string()}};
 
     let mut matrix: Vec<Option<usize>> = vec![Some(0); N];
+    let ind = |i: usize, j: usize| i*N + j;
     for alpha in 1..(p_sum_adjusted + 1)
     {
-        if alpha % 100 == 1 && alpha != 1{ println!(""); }
-        print!("{alpha:>5} | ");
+        if print_graph {
+            if alpha % 100 == 1 && alpha != 1 { println!(""); }
+            print!("{alpha:>8} | ");
+        }
         for i in 0..N
         {
             if i == 0
@@ -78,10 +112,10 @@ fn main() {
             else
             {
                 let sum_withnew = if alpha >= prices[i] {
-                    matrix[(alpha-prices[i]) * N + i - 1].map(|x| x + weights[i])
+                    matrix[ind(alpha-prices[i], i - 1)].map(|x| x + weights[i])
                 } else { None };
 
-                let sum_withoutnew = matrix[alpha*N + i - 1];
+                let sum_withoutnew = matrix[ind(alpha,i - 1)];
 
                 let sum_min = match sum_withnew {
                     None => sum_withoutnew,
@@ -94,11 +128,11 @@ fn main() {
                         }
                     }
                 };
-                
+
                 matrix.push(sum_min);
             }
 
-            print!("{:>5} ", num_or_inf(matrix[matrix.len() - 1]));
+            if print_graph { print!("{:>5} ", num_or_inf(matrix[matrix.len() - 1])); }
         } 
 
         let n_last_opt = matrix[matrix.len() -1];
@@ -108,22 +142,46 @@ fn main() {
                 if n_last <= B { alpha_max = alpha; }
             }
         }
-        println!(" | {alpha_max:>5}");
+        if print_graph { println!(" | {alpha_max:>5}"); }
     }
 
-    println!("");
-    println!("a_max: {alpha_max}");
-    println!(" => p_max: {}", num_or_inf(matrix[alpha_max*N + N - 1]));
-}
+    if print_graph {println!("") };
 
-fn print_objects(objects: &Vec<(usize, usize)>)
-{
-    print!("        | ");
-    for i in 0..objects.len() { print!("{:>5} ", i+1); }
-    println!("\n{:-<1$}", "", 10+6*objects.len());
-    print!("\nWeights | ");
-    for (w, _) in objects { print!("{w:>5} "); }
-    print!("\nPrices  | ");
-    for (_, p) in objects { print!("{p:>5} "); }
-    println!("");
+    println!("Solution: adjusted price sum {} with weight {}\n",
+        alpha_max,
+        num_or_inf(matrix[alpha_max*N + N - 1]));
+
+    // Backtracking of used objects
+    let mut cur_index = N - 1;
+    let mut cur_pricesum = alpha_max;
+    let mut orig_pricesum = 0;
+    let matrix_val = |row, col| matrix[row * N + col];
+    let mut markers = [false; N];
+
+    loop {
+        if cur_index == 0 {
+            if matrix_val(cur_pricesum, cur_index).is_some() && cur_pricesum != 0 {
+                markers[0] = true;
+                orig_pricesum += prices_orig[cur_index];
+            }
+            break;
+        } else {
+            if matrix_val(cur_pricesum, cur_index - 1) == matrix_val(cur_pricesum, cur_index) {
+                cur_index -= 1;
+            } else {
+                markers[cur_index] = true;
+                orig_pricesum += prices_orig[cur_index];
+                cur_pricesum -= prices[cur_index];
+                cur_index -= 1;
+            }
+        }
+    }
+
+    println!("Used objects: index (weight, adjusted price, original price)");
+    for (i, marker) in markers.iter().enumerate() {
+        if !marker {continue; }
+        println!("              {: >5} ({: >5}, {: >5}, {: >5})", i + 1, weights[i], prices[i], prices_orig[i]);
+    }
+    println!("          => Sum:    {: >5}  {: >5}  {: >5}", num_or_inf(matrix_val(alpha_max, N-1)), alpha_max, orig_pricesum);
+
 }
